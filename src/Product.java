ef2d60995a86c8c2eb8741bb68edadaed979eba2
HiltonPlant <balukuhilton4@gmail.com>
2025-11-28 22:43:19 +0300

    creating getters and display

--- src/Product.java
+++ src/Product.java
@@ -1,10 +1,16 @@
 public class Product {
-    public class Product {
         private String name;
         private double price;
         public Product(String name, double price) {
             this.name = name;
             this.price = price;
         }
+        public String getName() { return name; }
+        public double getPrice() { return price; }
 
+        // Small display helper
+        public void display() {
+            System.out.printf("%s - $%.2f%n", name, price);
+        }
     }
+
