231d5e1ff723e0d3466d1ea2cf1c6f538689aec8
HiltonPlant <balukuhilton4@gmail.com>
2025-11-28 22:23:42 +0300

    creating a constructor

--- src/Product.java
+++ src/Product.java
@@ -2,4 +2,9 @@ public class Product {
     public class Product {
         private String name;
         private double price;
-}
+        public Product(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+    }
